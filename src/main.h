5bde4181bb09c7515b7896ba2d8182b86b13fd23
Zain Ahmed <zainahmed1559@gmail.com>
2021-05-25 18:26:54 -0400

    cleaned up formatting

--- src/main.h
+++ src/main.h
@@ -6,45 +6,48 @@
 
 #define FURY // uncomment for Link Fury, comment for PE3
 
-#define CAN_BAUD 250000
-#define SERIAL_BAUD 921600
-#define ECU_TIMEOUT_MS 2000
-#define CAN_TIMEOUT_MS 2000
-#define COOLING_KILL_MS 10000
+#define CAN_BAUD                        250000
+#define SERIAL_BAUD                     921600
+#define ECU_TIMEOUT_MS                  2000
+#define CAN_TIMEOUT_MS                  2000
+#define COOLING_KILL_MS                 10000
 
 // Cooldown times in milliseconds
-#define FAN_COOLDOWN_MS 30000
-#define WATERPUMP_COOLDOWN_MS 60000
+#define FAN_COOLDOWN_MS                 30000
+#define WATERPUMP_COOLDOWN_MS           60000
 
 // Active duty cycles
-#define WATERPUMP_ACTIVE_DC 1.0f
-#define FAN_ACTIVE_DC 1.0f
+#define WATERPUMP_ACTIVE_DC             1.0f
+#define FAN_ACTIVE_DC                   1.0f
 
 // Cooldown duty cycles
-#define WATERPUMP_COOLDOWN_DC .7f
-#define FAN_COOLDOWN_DC .5f
+#define WATERPUMP_COOLDOWN_DC           0.7f
+#define FAN_COOLDOWN_DC                 0.5f
 
 // For cooling fan and pump
-#define PWM_PERIOD_US 100
+#define PWM_PERIOD_US                   100
 
 // Paramaters
-#define ENGINE_WARM_F 150       // Fahrenheit
-#define ENGINE_TEMP_DEADBAND 20 // Fahreneheit
+#define ENGINE_WARM_F                   150       // Fahrenheit
+#define ENGINE_TEMP_DEADBAND            20 // Fahreneheit
 
 // State Machine states
-#define safetyState 0
-#define engineOffState 1
-#define cooldownState 2
-#define coldRunningState 3
-#define hotRunningState 4
+#define safetyState                     0
+#define engineOffState                  1
+#define cooldownState                   2
+#define coldRunningState                3
+#define hotRunningState                 4
 
 // ethrottle safety
-#define ETHROTTLE_MAX_ERROR_COUNT 10
-#define APPS_VS_APPS_MAX_ERROR 10
-#define TPS_VS_TPS_MAX_ERROR 10
+#define ETHROTTLE_MAX_ERROR_COUNT       10
+#define APPS_VS_APPS_MAX_ERROR          10
+#define TPS_VS_TPS_MAX_ERROR            10
 
-#define APPS_VS_TPS_MAX_ERROR_COUNT 50
-#define APPS_VS_TPS_MAX_ERROR 20 // include idle offset
+#define APPS_VS_TPS_MAX_ERROR_COUNT     50
+#define APPS_VS_TPS_MAX_ERROR           20 // include idle offset
+
+// when TPS is above this value, APPS vs TPS error checking is active
+#define APPS_VS_TPS_ENABLE_THRESHOLD    32
 
 // Used only for printing purposes
 #ifdef PRINT_STATUS
